b4895a34bedda0a221f2484851ef69613aeb5ac9
ILYAS_SADIKI <sadikiilyas76@gmail.com>
2025-06-17 18:28:34 +0100

    Update head_def.py

--- head/head_def.py
+++ head/head_def.py
@@ -2,7 +2,6 @@ import sys
 import yaml
 sys.path.append('../../')
 from head.BaseHead import BaseHead
-from head.MagHead import MagHead
 
 
 class HeadFactory:
@@ -15,7 +14,7 @@ class HeadFactory:
     def __init__(self, head_type, head_conf_file, **args):
         self.head_type = head_type
         with open(head_conf_file) as f:
-            head_conf = yaml.load(f)
+            head_conf = yaml.load(f, Loader=yaml.FullLoader)
             self.head_param = head_conf[head_type]
         if 'num_class' in args:
             self.head_param['num_class'] = args['num_class']
@@ -26,15 +25,6 @@ class HeadFactory:
             feat_dim = self.head_param['feat_dim'] # dimension of the output features, e.g. 512 
             num_class = self.head_param['num_class'] # number of classes in the training set.
             head = BaseHead(feat_dim, num_class)
-        elif self.head_type == 'MagHead':
-            feat_dim = self.head_param['feat_dim'] # dimension of the output features, e.g. 512 
-            num_class = self.head_param['num_class'] # number
-            l_a = self.head_param['l_a']
-            u_a = self.head_param['u_a']
-            l_margin = self.head_param['l_margin']
-            u_margin = self.head_param['u_margin']
-            lamda = self.head_param['lamda']
-            head = MagHead(feat_dim, num_class, l_a, u_a, l_margin, u_margin, lamda)
         else:
             pass
         return head
